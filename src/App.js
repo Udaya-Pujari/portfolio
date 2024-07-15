787ca55609d63bed6f4b3d39adfdee83ac5f567a
Udaya-Pujari-194 <udayp@ttbsindia.com>
2024-07-15 18:26:42 +0530

    finalised to host...

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import AnimatedCursor from "react-animated-cursor"
-import v4 from "../src/slickvideos/v4.mp4"
+import v5 from "../src/slickvideos/v5.mp4"
+import v8 from "../src/slickvideos/v8.mp4"
 
 function App() {
   return (
     <>
       {/* <div className="bg-gradient-to-r from-sky-100 to-teal-100"> */}
-      <video className="video-background" src={v4} autoPlay loop muted />
+      <video className="video-background" src={v8} autoPlay loop muted />
 
       <div className="content-wrapper">
         <AnimatedCursor />
